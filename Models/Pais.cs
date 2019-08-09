bc4085671a4d3eed3da26022f562e52c424ebe4d
jean <jean.c.vasquez.r@gmail.com>
2019-08-09 16:04:46 +0200

    agregar repositorio
    backend con pruebas

---
+++ Models/Pais.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraduccionesLegales.WebApi.Models
+{
+    public class Pais
+    {
+        public int PaisId { get; set; }
+        
+        public String NombrePais { get; set; }
+        
+        public String Idioma { get; set; }
+
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
